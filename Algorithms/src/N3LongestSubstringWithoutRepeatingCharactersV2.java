d13b6587d2e8de8481944da9d2387843bb1e2cbe
xiao_mao <laurenli045@gmail.com>
2023-08-31 18:38:31 -0400

    better version longest substring without repeating characters

---
+++ Algorithms/src/N3LongestSubstringWithoutRepeatingCharactersV2.java
@@ -0,0 +1,32 @@
+/*
+Given a string s, find the length of the longest substring without repeating characters.
+This is a better solution; it beat 99.71% of other java online users in speed & 99.06% for memory usage
+ */
+
+public class N3LongestSubstringWithoutRepeatingCharactersV2 {
+    public static int lengthOfLongestSubstring(String s) {
+        int length = 0;
+
+        for(int right = 0, left = 0; right < s.length(); right++) {
+            int index = s.indexOf(s.charAt(right), left);
+            if(index != right) left = index + 1;
+            length = Math.max(length, right - left + 1);
+        }
+
+        return length;
+    }
+
+    public static void main (String[] args){
+        String str1 = "abcabcbb";
+        String str2 = "bbbbb";
+        String str3 = "pwwkew";
+        String str4 = " ";
+        String str5 = "au";
+
+        System.out.println(lengthOfLongestSubstring(str1));
+        System.out.println(lengthOfLongestSubstring(str2));
+        System.out.println(lengthOfLongestSubstring(str3));
+        System.out.println(lengthOfLongestSubstring(str4));
+        System.out.println(lengthOfLongestSubstring(str5));
+    }
+}
